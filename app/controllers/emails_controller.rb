0571dd05dfda5788338156711e97ac36a306ec49
Yury Voloshin <yvoloshin01@gmail.com>
2015-05-27 23:04:35 +0000

    Commit 3

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,6 +1,20 @@
 class EmailsController < ApplicationController
 
-	def index
-		@emails=Email.all
-	end
+def index
+  @emails=Email.all
+  @email=Email.new
+end
+
+def create
+  Email.create(email_params)
+  redirect_to root_path
+  end
+
+  private
+
+  def email_params
+    params.require(:email).permit(:name, :email)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Quiz2::Application.routes.draw do
 
   root 'emails#index'
+  resources :emails
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
