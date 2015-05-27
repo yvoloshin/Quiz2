be32a922e5309e6918ac02ad171cf66b7df276d7
Yury Voloshin <yvoloshin01@gmail.com>
2015-05-27 16:20:46 +0000

    Commit2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Quiz2::Application.routes.draw do
+
+  root 'emails#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
